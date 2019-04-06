a7368563d00d3566e842d97fe96db6d25a7b6291
maybezsqs <1821911148@qq.com>
2019-04-06 18:57:30 +0800

    first

---
+++ Line_sign/build/sq/CMakeFiles/sq.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sq.dir/main.cpp.o"
+  "../../bin/sq.pdb"
+  "../../bin/sq"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sq.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
